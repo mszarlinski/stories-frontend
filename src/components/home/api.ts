31c79ebfaa9f7b6016f15cbb77cf6b60126b5cd1
Maciej Szarliński <mszarlinski@gmail.com>
2021-01-22 16:06:22 +0100

    Authentication, public routes

--- src/components/home/api.ts
+++ src/components/home/api.ts
@@ -2,5 +2,5 @@ import { HttpClient } from "../../http/HttpClient";
 import { HomePageStories } from "./model";
 
 export const getStoriesForHomePage = () =>
-  HttpClient.get<HomePageStories>("/home/stories")
+  HttpClient.get<HomePageStories>("/public/home/stories")
     .then((r) => r.data.stories);
--- src/http/HttpClient.ts
+++ src/http/HttpClient.ts
@@ -2,8 +2,13 @@ import axios from "axios";
 import { TokenStore } from "../components/auth/context";
 
 export const HttpClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api",
-  headers: {
-    Authorization: "Bearer " + TokenStore.getToken(),
-  },
+  baseURL: process.env.REACT_APP_API_URL + "/api"
+});
+
+HttpClient.interceptors.request.use((config) => {
+  const token = TokenStore.getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
