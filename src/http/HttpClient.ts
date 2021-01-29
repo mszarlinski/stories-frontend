6909c2a768b37b63f4c40225b8a514b48c38c3f9
Maciej Szarliński <mszarlinski@gmail.com>
2021-01-29 22:00:38 +0100

    Fixed authentication - storing full user info in session storage

--- src/http/HttpClient.ts
+++ src/http/HttpClient.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { TokenStore } from "../components/auth/context";
+import { CurrentUser } from "../components/auth/context";
 
 export const HttpClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api"
+  baseURL: process.env.REACT_APP_API_URL + "/api",
 });
 
 HttpClient.interceptors.request.use((config) => {
-  const token = TokenStore.getToken();
+  const token = CurrentUser.get()?.token;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
